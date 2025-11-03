d9470746e1a1ed589ce1028d64a902754c6ddb54
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2025-11-03 13:59:19 +0300

    added the all code for it

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -6,6 +6,7 @@ import { DoctorRoutes } from "../modules/doctor/doctor.routes";
 import { doctorScheduleRoutes } from "../modules/doctorSchedule/doctorSchedule.routes";
 import { PatientRoutes } from "../modules/patient/patient.routes";
 import { PrescriptionRoutes } from "../modules/prescription/prescription.routes";
+import { ReviewRoutes } from "../modules/review/review.routes";
 import { ScheduleRoutes } from "../modules/schedule/schedule.routes";
 import { SpecialtiesRoutes } from "../modules/specialties/specialties.routes";
 import { userRoutes } from "../modules/user/user.router";
@@ -52,6 +53,10 @@ const moduleRoutes = [
   {
     path: "/prescription",
     route: PrescriptionRoutes,
+  },
+  {
+    path: "/reviwe",
+    route: ReviewRoutes,
   },
 ];
 
