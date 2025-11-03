0e3a8e6fed549638a70b369f0cadee185143cda5
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2025-11-03 20:27:42 +0300

    added the code

--- src/app/modules/appointment/appointment.controller.ts
+++ src/app/modules/appointment/appointment.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
-
-import pick from "../../helpers/pick";
 import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { IJWTPayload } from "../../types/common";
 import { AppointmentService } from "./appointment.service";
+
+import httpStatus from "http-status";
+import pick from "../../helpers/pick";
+import { appointmentFilterableFields } from "./appointment.constant";
 
 const createAppointment = catchAsync(
   async (req: Request & { user?: IJWTPayload }, res: Response) => {
@@ -64,8 +66,22 @@ const updateAppointmentStatus = catchAsync(
   }
 );
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, appointmentFilterableFields);
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+  const result = await AppointmentService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Appointment retrieval successfully",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AppointmentController = {
   createAppointment,
   getMyAppointment,
   updateAppointmentStatus,
+  getAllFromDB,
 };
